a9b4d1a5aef7b7609f6626dda2afdba468c0ba66
Matthew O'Boyle <oboyle.matthew@duke.edu>
2020-08-25 23:14:00 +0100

    Updates

---
+++ precache-manifest.2bd8cce4bb4d77e1546a6fa196c4da9a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69a7f4f470dd981d6179780587a1ccd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "564a91c7c4716b2e65b8",
+    "url": "/static/css/main.40d1e5c9.chunk.css"
+  },
+  {
+    "revision": "398cddc1b1f55cba8f23",
+    "url": "/static/js/2.99a2d22d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.99a2d22d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "564a91c7c4716b2e65b8",
+    "url": "/static/js/main.78c7f58f.chunk.js"
+  },
+  {
+    "revision": "2e2377b767ad0891f00e",
+    "url": "/static/js/runtime-main.528fb465.js"
+  },
+  {
+    "revision": "ce4127c7acf66573fa260278a9dc6b7b",
+    "url": "/static/media/Constitution cover cropped.ce4127c7.png"
+  },
+  {
+    "revision": "565c01742c244f3b47660f30395872d7",
+    "url": "/static/media/bottom_text.565c0174.png"
+  },
+  {
+    "revision": "512f3bc182e8d13b34e57f1265eada22",
+    "url": "/static/media/top_cropped.512f3bc1.png"
+  }
+]);
